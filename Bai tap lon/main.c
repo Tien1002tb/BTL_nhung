8cb728277b47f9dbc18a55a8e4989c64d769ea68
Tien1002tb <godsofmickey@gmail.com>
2024-03-18 20:56:49 +0700

    8:56 18/03

--- Bai tap lon/main.c
+++ Bai tap lon/main.c
@@ -27,10 +27,11 @@ void mansetting2(void);
 void Setting(void);
 
 int main(){
-	systick_init();// initialize the delay function (Must initialize)
+	gpio_Init();
+	systick_init();
 	lcd_i2c_init(1);
 	DHT11_Init();
-
+	mainmenu();
 
 }
 void gpio_Init(void){
@@ -47,96 +48,102 @@ void gpio_Init(void){
 	GPIO_Init(GPIOA,&GPIO_Structure);	
 }
 void mainmenu(void){
-	while(1){
-	systick_init();// initialize the delay function (Must initialize)
-	lcd_i2c_init(1);
-	if (GPIO_ReadInputDataBit(GPIO_PORT, OK) == 0){
-		congtru_tong += 1;			
-			if(congtru_tong == 3){
-				congtru_tong = 3;
-			}
-	if (GPIO_ReadInputDataBit(GPIO_PORT,BACK) == 0){
-			congtru_tong = congtru_tong - 1 ;
-			if(congtru_tong == 0){
-				congtru_tong = 0;
-			}
-	}		
-	switch(congtru_tong){
-		case 0:
+//	while(1){
+//	systick_init();// initialize the delay function (Must initialize)
+//	lcd_i2c_init(1);
+//	if (GPIO_ReadInputDataBit(GPIO_PORT, OK) == 0){
+//		congtru_tong = congtru_tong + 1;			
+//			if(congtru_tong == 3){
+//				congtru_tong = 3;
+//			}
+//		}
+//	if (GPIO_ReadInputDataBit(GPIO_PORT,BACK) == 0){
+//			congtru_tong = congtru_tong - 1 ;
+//			if(congtru_tong == 0){
+//				congtru_tong = 0;
+//			}
+//	}		
+//	switch(congtru_tong){
+//		case 0:
 			lcd_i2c_cmd(1,0x01); // Clear Display
 			lcd_i2c_msg(1 ,1, 0, "MAN HINH CHINH");
-			lcd_i2c_msg(1 ,2, 0, ">TIEP");	
-		case 1:
-			mannhietdo();
-		case 2:
-			mansetting1();
-			Setting();
-		case 3:
-			mansetting2();
-			Setting();
-		}
-	}
+			lcd_i2c_msg(1 ,2, 0, ">TIEP");
+			DelayMs(50);
 }
-}
-void mannhietdo(void){
-	lcd_i2c_cmd(1,0x01); // Clear Display
-	while(1){
-	DHT11_Read_Data(&RHI, &RHD, &TCI, &TCD);
-	sprintf(Temp1 ,"T:%u *C",TCI);
-	lcd_i2c_msg(1 ,1, 0, Temp1);
-	sprintf(Humi1 ,"H:%u %%",RHI);
-	lcd_i2c_msg(1 ,2, 0, Humi1);
-	DelayMs(50);
-}
-	}
-void mansetting1(void){
-	while(1){
-	sprintf(Temp1 ,"SET >T:%u *C", TCI_set);
-	lcd_i2c_msg(1 ,1, 0, Temp2);
-	sprintf(Humi1 ,"H:%u %%",RHI_set);
-	lcd_i2c_msg(1 ,2, 5, Humi2);
-	DelayMs(50);
-	}
-}
+//		break;
+//		case 1:
+//			mannhietdo();
+//		break;
+//		case 2:
+//			mansetting1();
+//			Setting();
+//		break;
+//		case 3:
+//			mansetting2();
+//			Setting();
+//		break;
+//		}
+//	}
+//}
+//void mannhietdo(void){
+//	lcd_i2c_cmd(1,0x01); // Clear Display
+//	while(1){
+//	DHT11_Read_Data(&RHI, &RHD, &TCI, &TCD);
+//	sprintf(Temp1 ,"T:%u *C",TCI);
+//	lcd_i2c_msg(1 ,1, 0, Temp1);
+//	sprintf(Humi1 ,"H:%u %%",RHI);
+//	lcd_i2c_msg(1 ,2, 0, Humi1);
+//	DelayMs(50);
+//}
+//	}
+//void mansetting1(void){
+//	while(1){
+//	sprintf(Temp1 ,"SET >T:%u *C", TCI_set);
+//	lcd_i2c_msg(1 ,1, 0, Temp2);
+//	sprintf(Humi1 ,"H:%u %%",RHI_set);
+//	lcd_i2c_msg(1 ,2, 5, Humi2);
+//	DelayMs(50);
+//	}
+//}
 
-void mansetting2(void){
-	while(1){
-	sprintf(Temp1 ,"SET T:%u *C", TCI_set);
-	lcd_i2c_msg(1 ,1, 0, Temp2);
-	sprintf(Humi1 ,">H:%u %%",RHI_set);
-	lcd_i2c_msg(1 ,2, 5, Humi2);
-	DelayMs(50);
-	}
-}
-void Setting(void){
-	while(1){
-	if (congtru_tong == 2){
-		if (GPIO_ReadInputDataBit(GPIO_PORT, CONG) == 0){ // nut tang nhiet do 
-				TCI_set = TCI_set + 1;
-					if (TCI_set == 90){
-							TCI_set = 90;
-					}
-			}
-		if (GPIO_ReadInputDataBit(GPIO_PORT, TRU) == 0){ // nut giam nhiet do 
-				TCI_set = TCI_set - 1;
-					if (TCI_set == 0){
-						TCI_set = 0;
-					}
-			}
-		}
-	if(congtru_tong == 3){
-		if (GPIO_ReadInputDataBit(GPIO_PORT, CONG) == 0){ // nut tang do am
-				RHI_set = RHI_set + 1;
-					if (RHI_set == 100){
-						RHI_set = 100;
-					}
-			}
-		if (GPIO_ReadInputDataBit(GPIO_PORT, TRU) == 0){ // nut giam do am
-				RHI_set = RHI_set - 1;
-					if (RHI_set == 0){
-						RHI_set = 0;
-					}
-			}
-	}
-	}
-}
+//void mansetting2(void){
+//	while(1){
+//	sprintf(Temp1 ,"SET T:%u *C", TCI_set);
+//	lcd_i2c_msg(1 ,1, 0, Temp2);
+//	sprintf(Humi1 ,">H:%u %%",RHI_set);
+//	lcd_i2c_msg(1 ,2, 5, Humi2);
+//	DelayMs(50);
+//	}
+//}
+//void Setting(void){
+//	while(1){
+//	if (congtru_tong == 2){
+//		if (GPIO_ReadInputDataBit(GPIO_PORT, CONG) == 0){ // nut tang nhiet do 
+//				TCI_set = TCI_set + 1;
+//					if (TCI_set == 90){
+//							TCI_set = 90;
+//					}
+//			}
+//		if (GPIO_ReadInputDataBit(GPIO_PORT, TRU) == 0){ // nut giam nhiet do 
+//				TCI_set = TCI_set - 1;
+//					if (TCI_set == 0){
+//						TCI_set = 0;
+//					}
+//			}
+//		}
+//	if(congtru_tong == 3){
+//		if (GPIO_ReadInputDataBit(GPIO_PORT, CONG) == 0){ // nut tang do am
+//				RHI_set = RHI_set + 1;
+//					if (RHI_set == 100){
+//						RHI_set = 100;
+//					}
+//			}
+//		if (GPIO_ReadInputDataBit(GPIO_PORT, TRU) == 0){ // nut giam do am
+//				RHI_set = RHI_set - 1;
+//					if (RHI_set == 0){
+//						RHI_set = 0;
+//					}
+//			}
+//	}
+//	}
+//}
