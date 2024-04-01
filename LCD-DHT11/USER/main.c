7261061e86e63d90bdaae5e63c4f83bb8d9c1b6d
Tien1002tb <godsofmickey@gmail.com>
2024-04-01 08:10:54 +0700

    1/4

--- LCD-DHT11/USER/main.c
+++ LCD-DHT11/USER/main.c
@@ -12,69 +12,46 @@
 #include "lcd_1602_drive.h"
 #include "dht11.h"
 
+
 #define GPIO_PORT GPIOA
 #define DEN  GPIO_Pin_0
 #define QUAT GPIO_Pin_1
-#define BUTTON_1 GPIO_Pin_3
-#define BUTTON_2 GPIO_Pin_4
-#define BUTTON_3 GPIO_Pin_5
-#define BUTTON_4 GPIO_Pin_6
+#define CONG GPIO_Pin_3
+#define TRU GPIO_Pin_4
+#define OK GPIO_Pin_5
+#define BACK GPIO_Pin_6
 
-GPIO_InitTypeDef GPIO_Structure;
-char  Temp[20], Humi[20];
-u8 tempset = 30, humiset = 20;
+GPIO_InitTypeDef GPIO_Structure; 
+int congtru_tong = 0;
 uint8_t RHI = 0,RHD = 0 , TCI = 0, TCD = 0;
-char line1[20], line2[20];
+u8 RHI_set = 0, TCI_set = 0;
+char Temp1[20], Temp2[20];
+char Humi1[20], Humi2[20];
+char Temp[20], Humi[20];
+
 void EXTI0_Config(void);
 void USART2_Config(void);
 void USART2_SendString(uint8_t *str);
 void gpio_Init(void);
-
+void mannhietdo(void);
+void mansetting1(void);
+void mansetting2(void);
+void Setting(void);
 
 void hienthi_LCD(void *p); //hien thi nhiet do , do am len LCD
 void GPIO_active(void *p); // hoat dong cua ngoai vi (den,quat)
-void Setting(void *p); // cai dat nhiet do, do am theo yeu cau 
 void GT_esp32(void *p); // truyen du lieu tu stm32 sang esp32
 
 int main(void){
+	gpio_Init();
+	DHT11_Init();
 	xTaskCreate(GT_esp32, (const char*)"Gtiep_esp32",128 , NULL,1, NULL);
 	xTaskCreate(hienthi_LCD, (const char*)"Display",128 , NULL,1, NULL);
 	xTaskCreate(GPIO_active, (const char*)"GPIO",128 , NULL, 1, NULL);
-	xTaskCreate(Setting, (const char*)"Setting",128 , NULL, tskIDLE_PRIORITY, NULL);
 	vTaskStartScheduler(); 
 	while(1){
 	}
 }
-
-
-// Phan hien thi
-void hienthi_LCD(void *p){
-	systick_init();// initialize the delay function (Must initialize)
-	lcd_i2c_init(1);
-	DHT11_Init();
-	while(1)	{	
-		DHT11_Read_Data(&RHI, &RHD, &TCI, &TCD);
-		sprintf(line1 ,"T:%u *C | H:%u %%", TCI, RHI);
-		sprintf(line2 ,"SET:%u*C | %u%%", tempset, humiset);	
-		lcd_i2c_msg(1 ,1, 0, line1);
-		lcd_i2c_msg(1 ,2, 0, line2);
-		DelayMs(500);
-	}
-	}
-//Dieu khien DEN , Quat 
-void GPIO_active(void *p){
-	while(1){
-		if(tempset <= TCI){
-			GPIO_SetBits( GPIO_PORT , DEN);	
-			GPIO_ResetBits(GPIO_PORT , QUAT);
-		}
-		if(tempset > TCI){
-			GPIO_SetBits( GPIO_PORT , QUAT);	
-			GPIO_ResetBits(GPIO_PORT , DEN);
-}
-	}
-}
-// nut nhan set nhiet do 
 
 void gpio_Init(void){
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
@@ -84,49 +61,126 @@ void gpio_Init(void){
 	GPIO_Structure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(GPIOA,&GPIO_Structure);	
 	
-	GPIO_Structure.GPIO_Pin = BUTTON_1 | BUTTON_2 | BUTTON_3 |BUTTON_4; // nut nhan 1
+	
+	GPIO_Structure.GPIO_Pin = CONG | TRU | OK | BACK ; // nut
 	GPIO_Structure.GPIO_Mode = GPIO_Mode_IPU;
 	GPIO_Structure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(GPIOA,&GPIO_Structure);	
 }
 
-void Setting(void *p){
-	gpio_Init();
-	
+void mainmenu(void){
 	while(1){
-		if (GPIO_ReadInputDataBit(GPIO_PORT, BUTTON_1) == 0){ // nut tang nhiet do 
-				tempset = tempset + 1;
-					if (tempset == 60){
-						tempset = 0;
-					}
+	systick_init();// initialize the delay function (Must initialize)
+	lcd_i2c_init(1);
+	if (GPIO_ReadInputDataBit(GPIO_PORT, OK) == 0){
+		congtru_tong = congtru_tong + 1;			
+			if(congtru_tong == 3){
+				congtru_tong = 3;
 			}
 		}
-	while(1){	
-		if (GPIO_ReadInputDataBit(GPIO_PORT, BUTTON_2) == 0){ // nut giam nhiet do 
-				tempset = tempset - 1;
-					if (tempset == 0){
-						tempset = 60;
-					}
+	if (GPIO_ReadInputDataBit(GPIO_PORT,BACK) == 0){
+			congtru_tong = congtru_tong - 1 ;
+			if(congtru_tong == 0){
+				congtru_tong = 0;
 			}
+	}		
+	switch(congtru_tong){
+		case 0:
+			lcd_i2c_cmd(1,0x01); // Clear Display
+			lcd_i2c_msg(1 ,1, 0, "MAN HINH CHINH");
+			lcd_i2c_msg(1 ,2, 0, ">TIEP");
+			DelayMs(50);
+		break;
+		case 1:
+			mannhietdo();
+		break;
+		case 2:
+			mansetting1();
+			Setting();
+		break;
+		case 3:
+			mansetting2();
+			Setting();
+		break;
 		}
+	}
+}
+void mannhietdo(void){
+	lcd_i2c_cmd(1,0x01); // Clear Display
 	while(1){
-		if (GPIO_ReadInputDataBit(GPIO_PORT, BUTTON_3) == 0){ // nut tang do am
-				humiset = humiset + 1;
-					if (humiset == 100){
-						humiset = 0;
-					}
-			}
-		}		
-	while(1){	
-		if (GPIO_ReadInputDataBit(GPIO_PORT, BUTTON_2) == 0){ // nut giam do am
-				humiset = humiset - 1;
-					if (humiset == 0){
-						humiset = 100;
-					}
-			}
+	DHT11_Read_Data(&RHI, &RHD, &TCI, &TCD);
+	sprintf(Temp1 ,"T:%u *C",TCI);
+	lcd_i2c_msg(1 ,1, 0, Temp1);
+	sprintf(Humi1 ,"H:%u %%",RHI);
+	lcd_i2c_msg(1 ,2, 0, Humi1);
+	DelayMs(50);
+}
+	}
+void mansetting1(void){
+	while(1){
+	sprintf(Temp1 ,"SET >T:%u *C", TCI_set);
+	lcd_i2c_msg(1 ,1, 0, Temp2);
+	sprintf(Humi1 ,"H:%u %%",RHI_set);
+	lcd_i2c_msg(1 ,2, 5, Humi2);
+	DelayMs(50);
 	}
 }
 
+void mansetting2(void){
+	while(1){
+	sprintf(Temp1 ,"SET T:%u *C", TCI_set);
+	lcd_i2c_msg(1 ,1, 0, Temp2);
+	sprintf(Humi1 ,">H:%u %%",RHI_set);
+	lcd_i2c_msg(1 ,2, 5, Humi2);
+	DelayMs(50);
+	}
+}
+void Setting(void){
+	while(1){
+	if (congtru_tong == 2){
+		if (GPIO_ReadInputDataBit(GPIO_PORT, CONG) == 0){ // nut tang nhiet do 
+				TCI_set = TCI_set + 1;
+					if (TCI_set == 90){
+							TCI_set = 90;
+					}
+			}
+		if (GPIO_ReadInputDataBit(GPIO_PORT, TRU) == 0){ // nut giam nhiet do 
+				TCI_set = TCI_set - 1;
+					if (TCI_set == 0){
+						TCI_set = 0;
+					}
+			}
+		}
+	if(congtru_tong == 3){
+		if (GPIO_ReadInputDataBit(GPIO_PORT, CONG) == 0){ // nut tang do am
+				RHI_set = RHI_set + 1;
+					if (RHI_set == 100){
+						RHI_set = 100;
+					}
+			}
+		if (GPIO_ReadInputDataBit(GPIO_PORT, TRU) == 0){ // nut giam do am
+				RHI_set = RHI_set - 1;
+					if (RHI_set == 0){
+						RHI_set = 0;
+					}
+			}
+	  }
+	}
+}
+
+//Dieu khien DEN , Quat 
+void GPIO_active(void *p){
+	while(1){
+		if(TCI_set <= TCI){
+			GPIO_SetBits( GPIO_PORT , DEN);	
+			GPIO_ResetBits(GPIO_PORT , QUAT);
+		}
+		if(RHI_set > TCI){
+			GPIO_SetBits( GPIO_PORT , QUAT);	
+			GPIO_ResetBits(GPIO_PORT , DEN);
+}
+	}
+}
 
 // truyen UART
 void EXTI0_Config(void){
